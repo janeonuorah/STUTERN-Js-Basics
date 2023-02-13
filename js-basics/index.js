2cb701fa05dc15036af4db032411453cf3fa50e7
Onyinyechi Onuorah <janeonuorah2000@gmail.com>
2023-02-13 12:24:02 +0100

    updated the js-basics file

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,5 +1,19 @@
-function Multiply(number1, number2){
-    console.log ("number1 * number2");
-}
+// function Multiply() {
+//     return ("5*8");
+// }
 
-Multiply(6,5)
+// console.log((Multiply()));
+
+let firstName = "Onyinyechi ";
+let lastName = "Onuorah";
+let stuternTrack = "Mobile Development";
+let skills = "Good leadership and Team work";
+let favoriteColors = "Black, grey and white";
+let yearOfCountryIndependence = 1960;
+let sentence =
+    "My name is " + firstName + lastName + "." + " I am learning " + stuternTrack + " in STUTERN cohort 14."
+    + " I am a sucker for " + skills + "." + " My favorite colors are " + favoriteColors + " and I am a Nigerian. My country's year of independence is "
+    + yearOfCountryIndependence + ". Thanks for reading this short write-up about me."
+console.log(sentence);
+
+
